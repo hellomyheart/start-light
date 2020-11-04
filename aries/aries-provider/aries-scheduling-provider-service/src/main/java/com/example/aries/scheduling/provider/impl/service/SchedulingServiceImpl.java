3349cc177ed78408e1cc372fa58f8c1d9b1b02a4
stephen shen <723809534@qq.com>
2020-11-04 22:31:18 +0800

    实现接口（导入依赖）

---
+++ aries/aries-provider/aries-scheduling-provider-service/src/main/java/com/example/aries/scheduling/provider/impl/service/SchedulingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.aries.scheduling.provider.impl.service;
+
+import com.example.aries.scheduling.provider.api.service.SchedulingService;
+
+/**
+ * @description 排班Service
+ * @className: SchedulingService
+ * @package: com.example.aries.scheduling.provider.impl.service
+ * @author: Stephen Shen
+ * @date: 2020/11/4 下午10:27
+ */
+public class SchedulingServiceImpl implements SchedulingService {
+}
