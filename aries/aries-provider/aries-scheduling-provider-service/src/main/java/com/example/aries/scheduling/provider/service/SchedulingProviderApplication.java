ce8aedf6c3924ba8e92294fd49555b3ef2a9e8d7
stephen shen <723809534@qq.com>
2020-11-04 22:26:22 +0800

    创建排班提供者service启动类

---
+++ aries/aries-provider/aries-scheduling-provider-service/src/main/java/com/example/aries/scheduling/provider/service/SchedulingProviderApplication.java
@@ -0,0 +1,18 @@
+package com.example.aries.scheduling.provider.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description
+ * @className: SchedulingProviderApplication
+ * @package: com.example.aries.scheduling.provider.service
+ * @author: Stephen Shen
+ * @date: 2020/11/4 下午10:25
+ */
+@SpringBootApplication
+public class SchedulingProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingProviderApplication.class,args);
+    }
+}
