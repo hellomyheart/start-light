eb24e89cc2130da8cc9d6b8a3007675f7a5a8a04
xr <1126099797@qq.com>
2020-11-05 19:22:04 +0800

    完成购物车第一版框架

---
+++ scorpio/scorpio_provider/src/main/java/com/scorpio/money/provider/LikeApplication.java
@@ -0,0 +1,14 @@
+package com.scorpio.money.provider;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * projectName: start-light
+ *
+ * @author: xr
+ * time: 2020/11/5 9:39
+ * description:
+ */
+@SpringBootApplication
+public class LikeApplication {
+}
