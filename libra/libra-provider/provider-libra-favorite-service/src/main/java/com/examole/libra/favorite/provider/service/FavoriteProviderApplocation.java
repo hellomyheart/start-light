48fd151dd62c57a6f2f07d846c13654daec14216
zhangbin <1213776889@qq.com>
2020-11-07 17:44:53 +0800

    搭架子呀搭架子

---
+++ libra/libra-provider/provider-libra-favorite-service/src/main/java/com/examole/libra/favorite/provider/service/FavoriteProviderApplocation.java
@@ -0,0 +1,14 @@
+package com.examole.libra.favorite.provider.service;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ProjectName: capricorn
+ * @Author: 张尚斌
+ * @Time: 2020/11/7 17:21
+ * @Description:
+ */
+@EnableDiscoveryClient
+
+public class FavoriteProviderApplocation {
+}
